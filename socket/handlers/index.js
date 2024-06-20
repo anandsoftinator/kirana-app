377e14fcffbe0c45e54461f8108505fd580db5ca
anandsoftinator <anand@softinator.com>
2024-06-20 19:36:02 +0530

    structured

---
+++ socket/handlers/index.js
@@ -0,0 +1,7 @@
+const logoutHandlerJs = require("./logoutHandler.js/index.js");
+const registerHandler = require("./registerHandler.js");
+
+module.exports = {
+  registerHandler,
+  logoutHandlerJs,
+};
--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,10 @@
-const registerOrderHandlers = require("./handlers/registerHandler.js");
+const { registerHandler } = require("./handlers");
 
 const userSockets = new Map();
 
 const onConnection = (socket) => {
   console.log(`A user connected with id ${socket.id}`);
-
-  registerOrderHandlers(io.socket, userSockets);
+  registerHandler(io.socket, userSockets);
 
   socket.on("disconnect", () => {
     console.log(`A user disconnected with id ${socket.id}`);
