1211891752d71bc594459e31b0304202041aa5d8
anandsoftinator <anand@softinator.com>
2024-06-25 15:00:22 +0530

    added get laslastMessage and conversation functionality

--- socket/handlers/conversationHandler/index.js
+++ socket/handlers/conversationHandler/index.js
@@ -38,5 +38,61 @@ module.exports = (io, socket) => {
       console.error("Error: create-conversation", error.message);
     }
   };
+
+  const getAllConversation = async ({ shopUUID }) => {
+    try {
+      const { data: conversationData, error: conversationError } =
+        await supabase
+          .from("conversation")
+          .select(
+            `
+              uuid,
+              userUUID,
+              user:userUUID (
+                uuid,
+                phone_number,
+                address,
+                latitude,
+                longitude,
+                last_online_at,
+                userImage,
+                name,
+                status
+              )
+            `
+          )
+          .eq("shopUUID", shopUUID);
+
+      if (conversationError) {
+        throw new Error(conversationError.message);
+      }
+
+      for (const conversation of conversationData) {
+        const { userUUID } = conversation;
+
+        const { data: messageData, error: messageError } = await supabase
+          .from("messages")
+          .select("*")
+          .or(
+            `and(senderUUID.eq.${shopUUID},receiverUUID.eq.${userUUID}),and(senderUUID.eq.${userUUID},receiverUUID.eq.${shopUUID})`
+          )
+          .order("created_at", { ascending: false })
+          .limit(1);
+
+        if (messageError) {
+          throw new Error(messageError.message);
+        }
+
+        conversation.lastMessage = messageData[0] || null;
+      }
+
+      console.log("conversationData", conversationData);
+      socket.emit("all-conversations", { data: conversationData });
+    } catch (error) {
+      console.error("Error: get-all-conversations", error.message);
+    }
+  };
+
   socket.on("create-conversation", createConversation);
+  socket.on("get-all-conversations", getAllConversation);
 };
--- socket/handlers/messageHandler/index.js
+++ socket/handlers/messageHandler/index.js
@@ -33,17 +33,77 @@ module.exports = (io, socket) => {
       if (error) {
         throw new Error(error.message);
       }
+
+      const { data: conversationData, error: conversationError } =
+        await supabase
+          .from("conversation")
+          .select(
+            `
+              uuid,
+              userUUID,
+              user:userUUID (
+                uuid,
+                phone_number,
+                address,
+                latitude,
+                longitude,
+                last_online_at,
+                userImage,
+                name,
+                status
+              )
+            `
+          )
+          .eq("uuid", convId);
+
+      if (conversationError) {
+        throw new Error(conversationError.message);
+      }
+
+      const { data: messageData, error: messageError } = await supabase
+        .from("messages")
+        .select("*")
+        .eq("conv_uuid", convId)
+        .order("created_at", { ascending: false })
+        .limit(1);
+
+      if (messageError) {
+        throw new Error(messageError.message);
+      }
+      conversationData[0].lastMessage = messageData;
+
       const receiverIds = await getSocketIds(receiverUUID);
       if (receiverIds) {
         receiverIds.forEach((receiverId) => {
           socket.to(receiverId.toString()).emit("get-message", { data });
+          socket
+            .to(receiverId.toString())
+            .emit("get-conversation", { data: conversationData });
         });
       }
+      socket.emit("get-conversation", { data: conversationData });
       socket.emit("get-message", { data });
     } catch (error) {
       console.error("Error: send-message", error.message);
     }
   };
 
+  const getAllMessages = async ({ convId }) => {
+    try {
+      const { data, error } = await supabase
+        .from("messages")
+        .select("*")
+        .eq("conv_uuid", convId);
+
+      if (error) {
+        throw new Error(error.message);
+      }
+      socket.emit("all-messages", { data });
+    } catch (error) {
+      console.error("Error: send-message", error.message);
+    }
+  };
+
   socket.on("send-message", sendGetMessage);
+  socket.on("get-all-messages", getAllMessages);
 };
